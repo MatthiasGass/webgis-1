a9fc732471da465e6f1c11fe54a651f8d3e54f3d
in227 <in227@uni-heidelberg.de>
2018-11-22 15:37:21 +0100

    ISSUE #13 - added Geoinstitut

---
+++ places/Geoinstitut.js
@@ -0,0 +1,9 @@
+add_polygon("Geoinstitut", 'red', [
+    // Hier die Koordinaten eintragen:
+    // Das Polygon kann aus beliebig vielen Punkten zusammengesetzt werden.
+    // Es sollte aber aus mindestens drei Punkten bestehen.
+    [49.41848363193122, 8.676621615886688],
+    [49.41848363193122, 8.677074909210205],
+    [49.41895299578881, 8.676608204841614],
+    [49.41846443851807, 8.676608204841614]
+]);
